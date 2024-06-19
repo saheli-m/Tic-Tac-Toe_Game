27e4776a796328ed16b9392f2f342cf9e5364e5c
Saheli Maity <sahelimaity501@gmail.com>
2024-06-19 12:58:17 +0530

    edit

--- app.js
+++ app.js
@@ -7,20 +7,27 @@ let msg = document.querySelector("#msg");
 let turnO = true;
 
 const winPatterns = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8]
+  [0, 1, 2, 'row-1'],
+  [3, 4, 5, 'row-2'],
+  [6, 7, 8, 'row-3'],
+  [0, 3, 6, 'col-1'],
+  [1, 4, 7, 'col-2'],
+  [2, 5, 8, 'col-3'],
+  [0, 4, 8, 'diag-1'],
+  [2, 4, 6, 'diag-2']
 ];
 
 const resetGame = () => {
   turnO = true;
   enableBoxes();
   msgContainer.classList.add("hide");
+  removeLineThrough();
+};
+
+const removeLineThrough = () => {
+  boxes.forEach(box => {
+    box.classList.remove('line-through', 'row-1', 'row-2', 'row-3', 'col-1', 'col-2', 'col-3', 'diag-1', 'diag-2');
+  });
 };
 
 boxes.forEach((box) => {
@@ -51,10 +58,18 @@ const enableBoxes = () => {
   }
 };
 
-const showWinner = (winner) => {
-  msg.innerText = `Congratulation! The Winner is ${winner}`;
+const showWinner = (winner, winningPattern) => {
+  msg.innerText = `Congratulations, the Winner is ${winner}!`;
   msgContainer.classList.remove("hide");
   disableBoxes();
+  addLineThrough(winningPattern);
+};
+
+const addLineThrough = (pattern) => {
+  const [index1, index2, index3, className] = pattern;
+  boxes[index1].classList.add('line-through', className);
+  boxes[index2].classList.add('line-through', className);
+  boxes[index3].classList.add('line-through', className);
 };
 
 const checkWinner = () => {
@@ -65,7 +80,7 @@ const checkWinner = () => {
 
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        showWinner(pos1Val);
+        showWinner(pos1Val, pattern);
       }
     }
   }
